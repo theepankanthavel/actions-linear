8ac19d8377d52e6acc1c6228454c49ba58d94c92
Theepan Kanthavel <theepan.kanthavel@joinlane.com>
2021-06-22 18:05:12 -0400

    more changes

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const nameToGreet = core.getInput('your-name');
+  const nameToGreet = core.getInput('your_name');
+  const linearAccessToken = core.getInput('linear_access_token');
+  console.log('got access token?', (linearAccessToken || '').length);
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const {payload} = github.context;
+  const payloadStr = JSON.stringify(github.context.payload, undefined, 2)
+
+  if(github.context.eventName === 'push') {
+    payload.commits.forEach(commit => console.log(commit.message));
+  }
+  console.log(`The event payload: ${payloadStr}`);
+  
 } catch (error) {
   core.setFailed(error.message);
 }
