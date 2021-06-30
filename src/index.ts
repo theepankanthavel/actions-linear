e23e6f41fd5447cd3e1e3efb2c41377347072ee1
Theepan Kanthavel <theepan.kanthavel@joinlane.com>
2021-06-30 12:10:58 -0400

    fix: refactor ref: FEATURE_COMPLETE,POD-396,POD-431

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,7 @@ import * as github from '@actions/github';
 import {WebhookPayload} from "@actions/github/lib/interfaces";
 import {insertLabelToIssue} from "./modules/linear";
 import githubApi from "./modules/github";
-import config from "./config";
-
-const NO_ACTION = 'NO_ACTION';
-const FEATURE_COMPLETE = 'FEATURE_COMPLETE';
-/**
- * Helper function to parse commit message and return issue ids
- * @param commitMessage
- * @return string[]
- */
-function parseIssueIds(commitMessage: string): {noAction: boolean, featureComplete: boolean, ids: string[]} {
-  const pattern = /^ref:\s?(.+)$/gmi;
-  const matches = pattern.exec(commitMessage);
-  if(!matches) {
-    return {
-      noAction: true,
-      featureComplete: false,
-      ids:  []
-    }
-  }
-  const ids = matches[1].split(',').map(v => v.trim());
-  return {
-    noAction: ids.includes(NO_ACTION),
-    featureComplete: ids.includes(FEATURE_COMPLETE),
-    ids: ids.filter(id => [NO_ACTION, FEATURE_COMPLETE].includes(id) === false)
-  };
-}
+import {parseIssueIds} from "./util";
 
 /**
  * Main function to run the modules action
@@ -54,45 +29,44 @@ async function main(): Promise<void> {
   console.log('payload', payloadStr);
 
 
-  const issueIdsForBranchLabel = new Set();
-  const issueIdsForVersionLabel = new Set();
+  const issueIds = parseIssueIds(payload.commit);
+  console.log('issue ids', issueIds);
+  // const issueIdsForVersionLabel = new Set();
 
-  const parts = payload.ref.split("refs/heads/");
-  const labelConf = config.labelConfigs.find((conf: any) => conf.branch === parts?.[1]);
-  if(!labelConf) return;
-  payload.commits.forEach((commit: any) => {
-    const parseIssues = parseIssueIds(commit.message);
-    console.log('parsed ids',  parseIssues);
-    if(parseIssues.noAction) return;
-    parseIssues.ids.forEach(issueId => {
-      issueIdsForBranchLabel.add(issueId);
-      if(parseIssues.featureComplete) {
-        issueIdsForVersionLabel.add(issueId);
-      }
-    });
+  // payload.commits.forEach((commit: any) => {
+  //   const parseIssues = parseIssueIds(commit.message);
+  //   console.log('parsed ids',  parseIssues);
+  //   if(parseIssues.noAction) return;
+  //   parseIssues.ids.forEach(issueId => {
+  //     issueIds.add(issueId);
+  //     if(parseIssues.featureComplete) {
+  //       issueIdsForVersionLabel.add(issueId);
+  //     }
+  //   });
+  // });
+
+  const packageJsonContent = await githubApiClient.getFileContent('package.json');
+  const packageJson = JSON.parse(packageJsonContent);
+  const tasks: Promise<void>[] = Object.keys(issueIds).map((issueId: string) => {
+    const labels = [`deployed-in-${branch}`];
+    if(issueIds[issueId].featureComplete) {
+      labels.push(`version-${packageJson.version}`);
+    }
+    return insertLabelToIssue(issueId, labels)
   });
 
-  const tasks: Promise<void>[] = Array.from(issueIdsForBranchLabel).map((issueId: string) =>
-    insertLabelToIssue(issueId, {
-      id: labelConf.id,
-      name: `deployed-in-${branch}`
-    })
-  );
-
-  if(issueIdsForVersionLabel.size > 0) {
-    const packageJsonContent = await githubApiClient.getFileContent('package.json');
-    const packageJson = JSON.parse(packageJsonContent);
-    Array.from(issueIdsForVersionLabel).forEach((issueId: string) =>
-      tasks.push(insertLabelToIssue(issueId, {
-        id: labelConf.id,
-        name: `version-${packageJson.version}`
-      })
-    ));
-  }
+  // if(issueIdsForVersionLabel.size > 0) {
+  //   const packageJsonContent = await githubApiClient.getFileContent('package.json');
+  //   const packageJson = JSON.parse(packageJsonContent);
+  //   Array.from(issueIdsForVersionLabel).forEach((issueId: string) =>
+  //     tasks.push(insertLabelToIssue(issueId, {
+  //       name: `version-${packageJson.version}`
+  //     })
+  //   ));
+  // }
 
   const result = await Promise.allSettled(tasks);
   console.log(result);
-
 }
 
 main().catch(err => core.setFailed(err));
---
+++ src/util.ts
@@ -0,0 +1,33 @@
+type IssueIds = {
+  [key: string]: {featureComplete: boolean}
+}
+
+const NO_ACTION = 'NO_ACTION';
+const FEATURE_COMPLETE = 'FEATURE_COMPLETE';
+
+/**
+ * Helper function to parse commit message and return issue ids
+ * @param commits
+ * @return string[]
+ */
+export function parseIssueIds(commits: {message: string}[]): IssueIds {
+  const collection: IssueIds = {};
+  const pattern = /^\s*ref:\s?(.+)$/gmi;
+
+  commits.forEach(({message}) => {
+    console.log(message)
+    const matches = pattern.exec(message);
+    console.log('match', matches);
+    if(!matches) return;
+    const ids = matches[1].split(',').map(v => v.trim());
+    console.log(ids)
+    if(ids.includes(NO_ACTION)) return;
+    const featureComplete = ids.includes(FEATURE_COMPLETE);
+    ids.forEach(id => {
+      collection[id] = Object.assign(collection[id] || {}, {
+        featureComplete: collection[id]?.featureComplete || featureComplete
+      });
+    });
+  });
+  return collection;
+}
